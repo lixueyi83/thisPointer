a9046b91ad587db62eb869368785b34049bb98f3
Li <lixueyi83@gmail.com>
2017-11-02 15:25:54 -0700

    added multithreading.cpp

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,10 +1,138 @@
 /*************************************************************************
-	> File Name: rvalue_referece.cpp
+	> File Name: multithreading.cpp
 	> Author: 
 	> Mail: 
 	> Created Time: Wed 18 Oct 2017 02:20:18 PM PDT
  ************************************************************************/
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <unistd.h>
+
 using namespace std;
+
+/* Three different ways of create Threeads: we can attach a callback with thread object. 
+*   
+*   std::thread thObj(<CALLBACKs>);
+*
+*   These CALLBACKs can be: 
+*       1) function pointer; 
+*       2) function objects; 
+*       3) lambda functions; 
+* */
+
+
+/*
+*  Creating a thread using Function Pointer 
+*/
+void thread_function()
+{
+    for(int i = 0; i < 10000; i++);
+        std::cout<<"thread function Executing"<<std::endl;
+}
+
+int main_1()  
+{
+    
+    std::thread threadObj(thread_function);
+    
+    usleep(1000); /* allow thread_function running first*/
+    
+    std::cout<<"display From MainThread"<<std::endl;
+        
+    threadObj.join();    
+    
+    std::cout<<"Exit of Main function"<<std::endl;
+    
+    return 0;
+}
+
+/*
+*  Creating a thread using Function Object 
+*/
+class DisplayThread
+{
+public:
+    void operator()()     
+    {
+        for(int i = 0; i < 10; i++)
+            std::cout<<"****** Thread Executing"<<std::endl;
+    }
+};
+ 
+int main_2()  
+{
+    DisplayThread functor();
+    //functor();
+    //std::thread threadObj((functor()));
+    std::thread threadObj( (DisplayThread()) );
+    for(int i = 0; i < 10; i++)
+        std::cout<<"------ From Main Thread "<<std::endl;
+    std::cout<<"Waiting For Thread to complete"<<std::endl;
+    threadObj.join();
+    std::cout<<"Exiting from Main Thread"<<std::endl;
+    return 0;
+}
+
+/*
+*  Creating a thread using Function Lambda function 
+*/
+int main_3()  
+{
+    int x = 9;
+    std::thread threadObj([]()
+        {
+            for(int i = 0; i < 10; i++)
+                std::cout<<"****** Thread Executing"<<std::endl;
+        });
+            
+    for(int i = 0; i < 10; i++)
+        std::cout<<"------ From Main Thread"<<std::endl;
+        
+    threadObj.join();
+    std::cout<<"Exiting from Main Thread"<<std::endl;
+    return 0;
+}
+
+
+class myFunctorClass
+{
+    public:
+        myFunctorClass (int x) : _x( x ) {}
+        int operator() (int y) 
+        { 
+            return _x + y; 
+            cout << "***********" << endl;
+        }
+        
+    private:
+        int _x;
+};
+ 
+int main()
+{
+    myFunctorClass addFive( 5 );
+    std::cout << addFive( 6 ) << endl;
+ 
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
