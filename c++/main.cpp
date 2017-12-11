c57d6f387c091748596251ced459039b9aaa8d29
Li <lixueyi83@gmail.com>
2017-12-11 13:59:35 -0800

    Added unordered_set examples

--- c++/main.cpp
+++ c++/main.cpp
@@ -1,5 +1,5 @@
 /*************************************************************************
-	> File Name: stl_unordered_map.cpp
+	> File Name: stl_unordered_set.cpp
 	> Author: 
 	> Mail: 
 	> Created Time: Wed 18 Oct 2017 02:20:18 PM PDT
@@ -12,6 +12,7 @@
 #include <map>
 #include <stack>
 #include <unordered_map>
+#include <unordered_set>
 #include <thread>
 #include <mutex>
 #include <algorithm>
@@ -24,198 +25,111 @@
 using namespace std;
 
 /*************************************************************************
-    Unordered_map provides a functionality of map i.e. it store the elements 
-    in key value pair and with unique key only. Unordered_map internally uses 
-    the hashing to achieve this. It internally uses a hash table to implement 
-    this hashing feature. In an unordered_map elements are stored in a key 
-    value pair combination. But elements are stored in arbitrary order unlike
-    associative containers where elements were stored in sorted order of keys.
-    
-    Advantage of Unordered_map:
-    The basic advantage of using unordered_map instead of associative map is 
-    the searching efficiency. In an unordered_map complexity to search for 
-    an element is O(1) if hash code are chosen efficiently.
-    
-    Different Ways to initialize an unordered_map:
-    unordered_map provides different overloaded constructors. We will use some 
-    of them to initialize an unordered_set in 3 different ways i.e.
-        1. Initializing through an initializer_list
-        2. Initializing through a iterable range
-        3. Initializing through an another map
-        
-    Find an element in unordered_map:  
-        iterator find ( const key_type& k );
-      
-    Insert elements in an unordered_map:
-        1. Inserting multiple elements in unordered_map through an initializer_list
-        2. Inserting a single element in an unordered_map and check result
+ * how to initialize an unordered_set in c++11 
+ *
+ * 1. initialize an unordered_set from an array
+ * 2. initialize an unordered_set by an initializer list 
+ * 3. initialize an unordered_set with a vector 
 */
 
-int main_init_insert_find()
+int main_init() 
 {
-	/* 1. Initialize an unordered_map through initializer_list */
-	std::unordered_map<std::string, int> wordMap(
-	{{ "First", 1 }, { "Second", 2 }, { "Third", 3 }});
+    /* 1. array */
+	int arr[] = { 2, 4, 6, 1, 3, 6, 8, 3, 2 };
+	std::unordered_set<int> setOfNum(arr, arr + sizeof(arr) / sizeof(int));
+	for (int val : setOfNum)
+		std::cout << val << "  ";
+	std::cout << std::endl;
  
-	/* Iterate over an unordered_map and display elements */
-	for (std::pair<std::string, int> element : wordMap)
-		std::cout << element.first << " :: " << element.second << std::endl;
+	/* 2. initializer_list */
+	std::unordered_set<int> setOfNum2( { 1, 2, 3, 1, 3, 4, 2 });
+	for (int val : setOfNum2)
+		std::cout << val << "  ";
+	std::cout << std::endl;
  
-	std::cout << "*******************" << std::endl;
- 
-	/* 2. Initialize an unordered_map through another range of elements of 
-	      type std::pair */
-	std::unordered_map<std::string, int> wordMap_2(wordMap.begin(),
-			wordMap.end());
- 
-	/* Iterate over an unordered_map and display elements */
-	for (std::pair<std::string, int> element : wordMap_2)
-		std::cout << element.first << " :: " << element.second << std::endl;
- 
-	std::cout << "*******************" << std::endl;
- 
-	/* 3. Initialize an unordered_map through other unordered_map */
-	std::unordered_map<std::string, int> wordMap_3(wordMap);
- 
-	/* Iterate over an unordered_map and display elements */
-	for (std::pair<std::string, int> element : wordMap_3)
-		std::cout << element.first << " :: " << element.second << std::endl;
-		
-	std::cout << "*******************" << std::endl;
-		
- 	/* 3. search element: Declare an iterator to unordered_map */
-	std::unordered_map<std::string, int>::iterator it;   
-	it = wordMap.find("First");
-	
-	if(it != wordMap.end())
-	{
-	    std::cout << "Element Found - " << it->first << "::" << it->second << endl; 
-	}
-	else 
-	{
-	    std::cout << "Element Not Found" << endl;
-	}
-	
-	std::cout << "*******************" << std::endl;
-	
-	/* 4. Insert elements through an initializer list */
-	wordMap.insert({ {"*First", 1}, {"*Second", 2}, {"*Third", 3} });
-	
-	/* Iterate over an unordered_map and display elements */
-	for (std::pair<std::string, int> element : wordMap)
-		std::cout << element.first << " :: " << element.second << std::endl;
- 
-	return 0;
+    /* 3. vector */
+	std::vector<int> vec( { 14, 5, 6, 7 });
+	std::unordered_set<int> setOfNum3(vec.begin(), vec.end());
+	for (int val : setOfNum3)
+		std::cout << val << "  ";
+	std::cout << std::endl;
 }
 
-/********************************************************************************
-* Erase element
-*  1. by key;
-*  2. by iterator;
-*
-********************************************************************************/
-int main_erase()
-{
-    cout << "-----------------------------------------------" << endl;
 
-    std::unordered_map<std::string, int> wordMap(
-        {
-            {"First", 1},
-            {"second", 2},
-            {"third", 3},
-            {"fourth", 4},
-            {"fifth", 5}
-        });
-
-    for(std::pair<std::string,int> element : wordMap)
-    {
-        std::cout << element.first << "::" << element.second << endl;
-    }
-
-    cout << "-----------------------------------------------" << endl;
-
-    /* erase element by key */
-    if(wordMap.erase("second") == 1)
-    {
-        std::cout << "\t *** erase second" << endl;
-    }
-
-    for(std::pair<std::string,int> element : wordMap)
-    {
-        std::cout << element.first << "::" << element.second << endl;
-    }
-    
-    cout << "-----------------------------------------------" << endl;
-
-    /********** by iterator ************/
-    std::unordered_map<string, int>::iterator it = wordMap.find("third");
-    if(it != wordMap.end())
-    {
-        wordMap.erase(it);
-        std::cout << "\t *** erased third" << endl;
-    }
-
-    cout << "-----------------------------------------------" << endl;
-
-    for(std::pair<std::string,int> element : wordMap)
-    {
-        std::cout << element.first << "::" << element.second << endl;
-    }
-    
-    cout << "-----------------------------------------------" << endl;
-
-    return 0;
-}
-
-int main() 
-{
-	/* Initialize an unordered_map through initializer_list */
-	std::unordered_map<std::string, int> wordMap( 
-        { 
-            { "First", 1 }, 
-            { "Second", 2 }, 
-            { "Third", 3 }, 
-            { "Fourth", 4 }, 
-            { "Fifth", 5 } });
- 
-	for (auto element : wordMap)
-		std::cout << element.first << " :: " << element.second << std::endl;
-    cout << "-------------------------------------------------" << endl;
-
-	/* Iterator pointing to first element in map */
-	std::unordered_map<std::string, int>::iterator it = wordMap.begin();
- 
-	/* Erase all element whose key starts with letter 'F' in an iteration */
-	while (it != wordMap.end()) 
-    {
-		/* Check if key's first character is F */
-		if (it->first[0] == 'F') 
-			/* erase() function returns the iterator of the next to last deleted element. */
-			it = wordMap.erase(it);
-		else
-			it++;
-	}
- 
-	for (auto element : wordMap)
-		std::cout << element.first << " :: " << element.second << std::endl;
- 
-	return 1;
-}
-
-/*************************************************************************************
- * map vs unordered_map:
+/*************************************************************************
+ * How to insert elements in an unordered_set 
+ * std::pair<iterator, bool> insert(const TYPE& val);
  *
- * http://thispointer.com/map-vs-unordered_map-when-to-choose-one-over-another/
-* ***********************************************************************************/
-
-
-
-
-
-
-
-
-
-
-
-
+ * If an element equivalent to passed element is already present in set, then new 
+ * element will not be inserted in set and ti will return the pair with following 
+ * values, 
+ *       - iterator wil point to the position of first matched equivalent element 
+ *       - bool flag will be false: ret.second == false 
+ *
+ * */
+ 
+ int main_insert() 
+ {
+	std::unordered_set<int> setOfNum;
+ 
+	/* 1. Inserting few elements in set */
+	setOfNum.insert(2);
+	setOfNum.insert(6);
+	setOfNum.insert(1);
+ 
+	/* Declare a pair of iterator and bool flag */
+	std::pair<std::unordered_set<int>::iterator, bool> result;
+ 
+	/* Try to insert a Duplicate Element, it will return a pair of iterator and bool */
+	result = setOfNum.insert(6);
+	if (result.second == false)
+		std::cout << "Failed to Insert 6" << std::endl;
+ 
+	/* Try to insert a another Element, it will return a pair of iterator and bool */
+	result = setOfNum.insert(7);
+	if (result.second)
+		std::cout << "Successfully Inserted 7" << std::endl;
+ 
+	for (int val : setOfNum)
+		std::cout << val << " ";
+	std::cout << std::endl;
+ 
+    /* 2. insert another container: 
+     * Create an another vector of ints */
+	std::vector<int> vec( { 3, 4, 1, 2, 9, 1, 3, 4, 5, 6 });
+	setOfNum.insert(vec.begin(), vec.end());
+	for (int val : setOfNum)
+		std::cout << val << " ";
+	std::cout << std::endl;
+ 
+	/* 3. Insert an initializer_list in existing  unordered set */
+	setOfNum.insert( { 2, 3, 6, 2, 15, 17, 11 });
+	for (int val : setOfNum)
+		std::cout << val << " ";
+	std::cout << std::endl;
+}
+ 
+/**********************************************************************************
+ * How to search an element in unordered_set
+ * std::unordered_set provides a member function find () that accepts an element 
+ * of same type as set and search for that element in set.
+ * */ 
+ 
+ int main() 
+ {
+	std::unordered_set<int> setOfNum( { 1, 2, 3, 1, 3, 4, 2 });
+	std::unordered_set<int>::const_iterator it = setOfNum.find(4);
+ 
+	if (it != setOfNum.end()) 
+    {
+		std::cout << "4 exist in the set" << std::endl;
+	}
+ 
+	it = setOfNum.find(9);
+ 
+	if (it == setOfNum.end()) 
+    {
+		std::cout << "9 dont exist in the set" << std::endl;
+	}
+ 
+}
